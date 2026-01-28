779eef148583a2de3652ecd74b0faab64d48f408
Aditya Kulkarni <vip.aditya.k@gmail.com>
2026-01-28 18:55:44 +0530

    Name chaneg

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from app.store import jobs
 from fastapi import HTTPException
 
-app = FastAPI(title="Clipix MVP")
+app = FastAPI(title="Celer Clips MVP")
 
 # Add CORS middleware
 app.add_middleware(

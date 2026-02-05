e220c3a2e98b21610572c7775700e8391f3cbca1
Aditya Kulkarni <vip.aditya.k@gmail.com>
2026-02-05 13:45:58 +0530

    minor fix

--- app/core/memory_limiter.py
+++ app/core/memory_limiter.py
@@ -1,35 +1,33 @@
-import resource
 import os
 import subprocess
 
 def set_memory_limit():
-    """Set memory limit for current process (512MB)"""
+    """Set memory limit for current process (Windows compatible)"""
     try:
-        # Set virtual memory limit to 512MB
-        resource.setrlimit(resource.RLIMIT_AS, (512 * 1024 * 1024, 512 * 1024 * 1024))
+        if os.name != 'nt':  # Unix/Linux only
+            import resource
+            resource.setrlimit(resource.RLIMIT_AS, (512 * 1024 * 1024, 512 * 1024 * 1024))
     except:
-        pass  # Ignore if not supported on platform
+        pass
 
 def run_ffmpeg_with_limits(cmd, timeout=300):
-    """Run FFmpeg with memory and CPU limits"""
+    """Run FFmpeg with memory and CPU limits (cross-platform)"""
     def preexec_fn():
-        # Set memory limit for child process
         try:
-            resource.setrlimit(resource.RLIMIT_AS, (512 * 1024 * 1024, 512 * 1024 * 1024))
-            # Set CPU time limit (5 minutes)
-            resource.setrlimit(resource.RLIMIT_CPU, (300, 300))
-            # Lower process priority
-            os.nice(10)
+            if os.name != 'nt':  # Unix/Linux only
+                import resource
+                resource.setrlimit(resource.RLIMIT_AS, (512 * 1024 * 1024, 512 * 1024 * 1024))
+                resource.setrlimit(resource.RLIMIT_CPU, (300, 300))
+                os.nice(10)
         except:
             pass
     
     # Add memory-specific FFmpeg options
     memory_limited_cmd = cmd + [
-        "-threads", "1",  # Single thread to reduce memory
-        "-thread_queue_size", "512",  # Smaller queue
+        "-thread_queue_size", "512",
     ]
     
-    if os.name != 'nt':  # Unix/Linux only
+    if os.name != 'nt':  # Unix/Linux
         return subprocess.Popen(
             memory_limited_cmd,
             stdout=subprocess.PIPE,
@@ -37,7 +35,7 @@ def run_ffmpeg_with_limits(cmd, timeout=300):
             text=True,
             preexec_fn=preexec_fn
         )
-    else:  # Windows
+    else:  # Windows - no resource limits available
         return subprocess.Popen(
             memory_limited_cmd,
             stdout=subprocess.PIPE,
